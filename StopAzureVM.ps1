caaf8780a8ec92bccf891f7819369aa5d2fb5de7
onag-fr <74780821+onag-fr@users.noreply.github.com>
2021-10-26 16:39:08 +0200

    Update StopAzureVM.ps1

--- StopAzureVM.ps1
+++ StopAzureVM.ps1
@@ -8,7 +8,7 @@
     Powershell Version :    5 or higher
 #>
 
-workflow StopVmScript
+workflow StopAzureVM
 {
 #######################################################################
 ## Set parameters
